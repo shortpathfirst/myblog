f88fc21b2d1823afc183aace9f59afd33156576c
shortpathfirst <andrea.zurli94@gmail.com>
2025-05-16 18:28:15 +0200

    Removed view transition temporary

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { getBlogPosts } from '@/lib/posts'
 import BlogPosts from '@/components/blogposts/blogposts'
-import { unstable_ViewTransition as ViewTransition } from 'react'
+// import { unstable_ViewTransition as ViewTransition } from 'react'
 
 const Page = async () => {
     const blogdata = await getBlogPosts();
 
     return (
-        <ViewTransition>
+        // <ViewTransition>
             <div className='container'>
                 <section>
                     <h1>Blog page</h1>
                     <BlogPosts blogData={blogdata}></BlogPosts>
                 </section>
             </div>
-        </ViewTransition>
+       
     )
 }
 
