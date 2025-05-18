71b9674b6f055157574f0facabef4066978e4408
shortpathfirst <andrea.zurli94@gmail.com>
2025-05-18 21:29:19 +0200

    Set for production

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const isProd = process.env.NODE_ENV === 'production';
-// export const baseUrl = process.env.BASE_URL || 'https://github.com/shortpathfirst'
-export const baseUrl = process.env.BASE_URL || 'https://localhost:3000'
+export const baseUrl = process.env.BASE_URL || 'https://github.com/shortpathfirst'
+// export const baseUrl = process.env.BASE_URL || 'https://localhost:3000'
