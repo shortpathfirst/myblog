70171e9f9c5f0697cf1a094f397d68873d340b0f
shortpathfirst <andrea.zurli94@gmail.com>
2025-06-08 19:34:30 +0200

    Correcting image paths

--- src/components/blogposts/blogposts.tsx
+++ src/components/blogposts/blogposts.tsx
@@ -12,7 +12,7 @@ const BlogPosts = ({ blogData }: {
                 blogData.map((post, i) => (
                     <BlogPostCard
                         key={i}
-                        imageSrc={post.metadata.imageUrl ?? "/file.svg"}
+                        imageSrc={post.metadata.imageUrl ?? "/myblog/file.svg"}
                         title={post.metadata.title}
                         href={`/blog/${post.slug}`}
                         description={post.metadata.description}
